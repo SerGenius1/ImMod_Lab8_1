d352a60765bedb188d611d8a156bef2dc9bbe308
SerGenius1 <s.burnashev03@gmail.com>
2024-04-30 18:23:02 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.bt_Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_Answer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(105, 52);
+            this.input.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(825, 86);
+            this.input.TabIndex = 0;
+            this.input.Text = "Введите свой вопрос";
+            // 
+            // bt_Start
+            // 
+            this.bt_Start.Cursor = System.Windows.Forms.Cursors.No;
+            this.bt_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_Start.Location = new System.Drawing.Point(364, 400);
+            this.bt_Start.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bt_Start.Name = "bt_Start";
+            this.bt_Start.Size = new System.Drawing.Size(227, 73);
+            this.bt_Start.TabIndex = 1;
+            this.bt_Start.Text = "Generate";
+            this.bt_Start.UseVisualStyleBackColor = true;
+            this.bt_Start.Click += new System.EventHandler(this.bt_Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(100, 241);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ответ:";
+            // 
+            // label_Answer
+            // 
+            this.label_Answer.AutoSize = true;
+            this.label_Answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Answer.Location = new System.Drawing.Point(213, 241);
+            this.label_Answer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Answer.Name = "label_Answer";
+            this.label_Answer.Size = new System.Drawing.Size(0, 29);
+            this.label_Answer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label_Answer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_Start);
+            this.Controls.Add(this.input);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button bt_Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_Answer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        private Dictionary<string, string> previousAnswers;
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            previousAnswers = new Dictionary<string, string>();
+        }
+
+        private void bt_Start_Click(object sender, EventArgs e)
+        {
+            string question = input.Text;
+            //Приведение всех букв к нижнему регистру и удаление всей пунктуации, чтобы она не прияла на содержание вопроса
+            question = question.ToLower();
+            question = Regex.Replace(question, "\\p{P}", string.Empty);
+            question = Regex.Replace(question, " ", string.Empty);
+
+            if (previousAnswers.ContainsKey(question))
+            {
+                label_Answer.Text = previousAnswers[question];
+            }
+            else
+            {
+                int randomNumber = random.Next(2);
+
+                if (randomNumber == 0)
+                {
+                    label_Answer.Text = "Yes";
+                }
+                else
+                {
+                    label_Answer.Text = "No";
+                }
+                previousAnswers[question] = label_Answer.Text;
+            }
+        }
+    }
+}
